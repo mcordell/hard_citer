bcf90ab666133a2688e87adee47723638072a3f2
Michael Cordell <surpher@gmail.com>
2013-08-15 11:47:16 -0700

    cleaning hardciter

--- lib/hard_citer.rb
+++ lib/hard_citer.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
-require 'stringio'
 require 'bibtex'
 require 'citeproc'
-require_relative 'cite_match'
-require_relative 'styler'
 require File.expand_path("../hardciter/configuration.rb", __FILE__)
+require File.expand_path("../hardciter/exceptions.rb", __FILE__)
 require File.expand_path("../hardciter/citer.rb", __FILE__)
+require File.expand_path("../hardciter/styler.rb", __FILE__)
+require File.expand_path("../hardciter/cite_match.rb", __FILE__)
+require File.expand_path("../hardciter/library.rb", __FILE__)
 
-HardCiter::Citer.new()
 
 
 
