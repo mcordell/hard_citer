47125cf3eb07c1eb41be8cb8d5e28479fe6417b4
Michael Cordell <surpher@gmail.com>
2013-08-16 10:45:42 -0700

    integrated citematch into hardciter module

--- lib/hardciter/cite_match.rb
+++ lib/hardciter/cite_match.rb
@@ -1,11 +1,17 @@
 # encoding: utf-8
-class CiteMatch
-  attr_accessor :bib_number, :citation, :in_cite_text, :key
+module HardCiter  
+  class CiteMatch
+    attr_accessor :bib_number, :citation, :in_cite_text, :key
 
-  def initialize(key, citation = nil, in_cite_text = nil)
-    @key = key
-    @citation = citation
-    @in_cite_text = in_cite_text
+    def initialize(key, citation = nil, in_cite_text = nil)
+      @key = key
+      @citation = citation
+      @in_cite_text = in_cite_text
+    end
+
+    def key
+      
+    end
   end
 end
 
