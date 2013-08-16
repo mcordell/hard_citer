ee002dc96574b13f3e2d3632c70c6dcbf9e87bcd
Michael Cordell <surpher@gmail.com>
2013-08-16 11:04:07 -0700

    requires in main hard_citer file

--- lib/hard_citer.rb
+++ lib/hard_citer.rb
@@ -1,12 +1,14 @@
 # encoding: utf-8
 require 'bibtex'
 require 'citeproc'
-require File.expand_path("../hardciter/configuration.rb", __FILE__)
-require File.expand_path("../hardciter/exceptions.rb", __FILE__)
-require File.expand_path("../hardciter/citer.rb", __FILE__)
-require File.expand_path("../hardciter/styler.rb", __FILE__)
+require File.expand_path("../hardciter/bibliography.rb", __FILE__)
 require File.expand_path("../hardciter/cite_match.rb", __FILE__)
+require File.expand_path("../hardciter/citer.rb", __FILE__)
+require File.expand_path("../hardciter/configuration.rb", __FILE__)
+require File.expand_path("../hardciter/document.rb", __FILE__)
+require File.expand_path("../hardciter/exceptions.rb", __FILE__)
 require File.expand_path("../hardciter/library.rb", __FILE__)
+require File.expand_path("../hardciter/styler.rb", __FILE__)
 
 
 
