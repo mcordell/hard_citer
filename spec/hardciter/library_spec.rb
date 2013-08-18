9d3fb4a573b27c4122eb7e0ff36495b05b383a64
Michael Cordell <surpher@gmail.com>
2013-08-18 12:32:52 -0700

    library specs for a bibtex library

--- spec/hardciter/library_spec.rb
+++ spec/hardciter/library_spec.rb
@@ -1,21 +1,50 @@
 require 'spec_helper'
 require 'bibtex'
+require 'lib/hardciter/library'
 
-describe Library do
-#  describe "when library has no records" do
-#    let(:library)=Library.new
-#    subject(library)
-#    describe "test bibtex" do
-#      b=BibTeX.open('./examples/test.bib')
-#      it { b['@book'].length == 2 }
-#    end
-#
-#    describe "getting the keys should return nil" do
-#      library.get_keys should be_nil
-#    end
-#
-#    describe "getting any key should return nothing" do
-#      library[:nothign] should be_nil
-#    end
-#  end
+describe HardCiter::BibTexLibrary do
+
+  let(:valid_bibtex_path) { File.expand_path("../../../examples/example_bib.bib", __FILE__) }
+
+  let(:bibtex_library) { HardCiter::BibTexLibrary.new(valid_bibtex_path) }
+
+  subject { bibtex }
+
+  describe "initialization" do
+    describe "with a valid path to a bib file" do
+      it "should initalize its bibtex to a BibTeX::Bibliography object" do
+        library = HardCiter::BibTexLibrary.new(valid_bibtex_path)
+        library.bibtex.should be_kind_of(BibTeX::Bibliography)
+      end
+    end
+  end
+
+  describe "getting a citation that exists in the bibtex library" do
+    before do
+      bibtex_library.bibtex = BibTeX.parse <<-END
+      @book{pickaxe,
+        address = {Raleigh, North Carolina},
+        author = {Thomas, Dave and Fowler, Chad and Hunt, Andy},
+        publisher = {The Pragmatic Bookshelf},
+        series = {The Facets of Ruby},
+        title = {Programming Ruby 1.9: The Pragmatic Programmer's Guide},
+        year = {2009}
+      }
+      END
+    end
+
+    describe "using the hash brackets" do
+      it "should return a BibTex::Entry" do
+        bibtex_library["pickaxe"].should be_kind_of(BibTeX::Entry)
+      end
+    end
+  end
+
+  describe "trying to get a citation that doesn't exist in the BibTex library" do
+
+    describe "using the hash brackets" do
+      it { bibtex_library["not-valid"].should be_nil }
+    end
+    
+  end
 end
