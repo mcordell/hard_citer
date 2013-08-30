7e8a7b505cc1d30ebbac8aa700beb93608f54f07
Michael Cordell <surpher@gmail.com>
2013-08-30 13:12:55 -0700

    basic bib key matching in parser line function with tests

--- lib/hardciter/parser.rb
+++ lib/hardciter/parser.rb
@@ -7,9 +7,30 @@ module HardCiter
       @bib_key = bib_key ? bib_key : HardCiter.configuration.bibliography_key
       @citation_key = citation_key ? citation_key : HardCiter.configuration.citation_key
     end
+    
+    def has_bibliography_key?(line)
+      line =~ @bib_key
+    end
+
+    def create_bib_match(line)
+      match_data = @bib_key.match(line)
+      match = HardCiter::IntextMatch.new()
+      match.type = HardCiter::BIBLIOGRAPHY_OUT_MATCH
+      match.position = match_data.begin(0) 
+      match.regex_match = match_data.to_s
+      match
+    end
+
+    def find_intext_citations(line)
+      nil
+    end
 
     def parse_line(line)
-      return []
+      if has_bibliography_key?(line)
+        create_bib_match(line)
+      else
+        find_intext_citations(line)
+      end
     end
   end
 end
--- spec/hardciter/parser_spec.rb
+++ spec/hardciter/parser_spec.rb
@@ -4,17 +4,18 @@ require 'spec_helper'
 module HardCiter
   describe Parser do
     let(:parser) { Parser.new() }
+    let(:default_bib_key) { HardCiter.configuration.bibliography_key }
+    let(:default_bib_string) { "{papers2_bibliography}" }
+    let(:default_citation_key) { HardCiter.configuration.citation_key }
 
     describe "initialization" do 
       context "when bibkey is not provided" do 
         it "should set its bibkey to the default configuration" do 
-          default_bibliography_key = HardCiter.configuration.bibliography_key
-          parser.bib_key.should be default_bibliography_key
+          parser.bib_key.should be default_bib_key
         end
       end
       context "when intext_key is not provided" do 
         it "should set its intext_key to the default configuration" do 
-          default_citation_key = HardCiter.configuration.citation_key
           parser.citation_key.should be default_citation_key
         end
       end
@@ -39,14 +40,26 @@ module HardCiter
         before do
           @no_intext_citations = "this line has no intext citations"
         end
-        it "should return an empty array" do
+        it "should return nil" do
           result = parser.parse_line(@no_intext_citations)
-          result.should be_empty
+          result.should be_nil
         end
       end
 
-      context "line has key that matches bibliography output" do
-        it "should return an IntextMatch with that position"
+      context "with line that has a bibliography key starting at the 3rd char" do
+        before do
+          bib_line = "012"+default_bib_string
+          @result = parser.parse_line(bib_line)
+        end
+        it "should return an IntextMatch" do
+          @result.should be_a_kind_of(HardCiter::IntextMatch)
+        end
+        it "should return an IntextMatch with a type of bibliography" do
+          @result.type.should be HardCiter::BIBLIOGRAPHY_OUT_MATCH
+        end
+        it "should return an IntextMatch with position 3" do 
+          @result.position.should be 3
+        end
       end
     end
   end
