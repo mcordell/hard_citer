e1ed9f5ade53f070ef3fd1b6e0a05f7f98ae17b3
Michael Cordell <surpher@gmail.com>
2013-08-30 18:35:01 -0700

    initialization for intext match

--- lib/hardciter/intext_match.rb
+++ lib/hardciter/intext_match.rb
@@ -4,5 +4,11 @@ module HardCiter
   INTEXT_CITATION_MATCH = 1 
   class IntextMatch
     attr_accessor :position, :type, :regex_match
+
+    def initialize(position=nil, regex_match=nil, type=nil)
+      @position = position
+      @regex_match = regex_match
+      @type = type
+    end
   end
 end
