2172f4d333384680eda8d30f21757bdfaa8f220d
Michael Cordell <surpher@gmail.com>
2013-09-01 08:46:10 -0700

    parser and tests functional

--- lib/hardciter/parser.rb
+++ lib/hardciter/parser.rb
@@ -14,20 +14,30 @@ module HardCiter
 
     def create_bib_match(line)
       match_data = @bib_key.match(line)
+      create_match(match_data, HardCiter::BIBLIOGRAPHY_OUT_MATCH)
+    end
+
+    def create_citation_match(match_data)
+      create_match(match_data, HardCiter::INTEXT_CITATION_MATCH)
+    end
+
+    def create_match(match_data,type)
       match = HardCiter::IntextMatch.new()
-      match.type = HardCiter::BIBLIOGRAPHY_OUT_MATCH
       match.position = match_data.begin(0) 
+      match.type = type
       match.regex_match = match_data.to_s
       match
     end
 
     def find_intext_citations(line)
-      nil
+      line.enum_for(:scan, @citation_key).map do
+        create_citation_match(Regexp.last_match)
+      end
     end
 
     def parse_line(line)
       if has_bibliography_key?(line)
-        create_bib_match(line)
+        [create_bib_match(line)]
       else
         find_intext_citations(line)
       end
--- spec/hardciter/parser_spec.rb
+++ spec/hardciter/parser_spec.rb
@@ -5,8 +5,9 @@ module HardCiter
   describe Parser do
     let(:parser) { Parser.new() }
     let(:default_bib_key) { HardCiter.configuration.bibliography_key }
-    let(:default_bib_string) { "{papers2_bibliography}" }
     let(:default_citation_key) { HardCiter.configuration.citation_key }
+    let(:bib_key_example) { "{papers2_bibliography}" }
+    let(:citation_key_example) { "{charles:19jk}" }
 
     describe "initialization" do 
       context "when bibkey is not provided" do 
@@ -42,23 +43,58 @@ module HardCiter
         end
         it "should return nil" do
           result = parser.parse_line(@no_intext_citations)
-          result.should be_nil
+          result.should be_empty
         end
       end
 
-      context "with line that has a bibliography key starting at the 3rd char" do
+      context "with line that has a bibliography key starting at the 3rd position" do
         before do
-          bib_line = "012"+default_bib_string
+          bib_line = "012"+bib_key_example
           @result = parser.parse_line(bib_line)
         end
-        it "should return an IntextMatch" do
-          @result.should be_a_kind_of(HardCiter::IntextMatch)
+        it "should return an array with an IntextMatch" do
+          @result[0].should be_a_kind_of(HardCiter::IntextMatch)
         end
         it "should return an IntextMatch with a type of bibliography" do
-          @result.type.should be HardCiter::BIBLIOGRAPHY_OUT_MATCH
+          @result[0].type.should be HardCiter::BIBLIOGRAPHY_OUT_MATCH
         end
         it "should return an IntextMatch with position 3" do 
-          @result.position.should be 3
+          @result[0].position.should be 3
+        end
+      end
+
+      context "with line that has an in-text citation at the 5th position" do
+        before do 
+          bib_line = "01234"+citation_key_example
+          @result = parser.parse_line(bib_line)
+        end
+        it "should return an array with an IntextMatch" do
+          @result[0].should be_a_kind_of(HardCiter::IntextMatch)
+        end
+        it "should return an IntextMatch with a type of intext citation" do
+          @result[0].type.should be HardCiter::INTEXT_CITATION_MATCH
+        end
+        it "should return an IntextMatch with position 3" do 
+          @result[0].position.should be 5
+        end
+      end
+
+      context "with line that has multiple citations" do
+        before do
+          multi_line = "01234{678:01}23{second:123}"
+          @result = parser.parse_line(multi_line)
+        end
+        it "should return an array with two intext-citations" do 
+          @result.length.should eq 2
+          @result.each { |r| r.should be_a_kind_of(HardCiter::IntextMatch) }
+        end
+        it "should return the first citation as the first intext" do
+          @result[0].regex_match.should eq "{678:01}"
+          @result[0].position.should be 5
+        end
+        it "should return the second citation as the second intext" do
+          @result[1].regex_match.should eq "{second:123}"
+          @result[1].position.should be 15 
         end
       end
     end
