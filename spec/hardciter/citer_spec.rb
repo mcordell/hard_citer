21f6d81df6cf32552ffa59e06ba61a662f7703cf
Michael Cordell <surpher@gmail.com>
2013-09-03 12:22:00 -0700

    remove the old specs that are now covered by the new group_matches spec

--- spec/hardciter/citer_spec.rb
+++ spec/hardciter/citer_spec.rb
@@ -96,7 +96,7 @@ module HardCiter
             @second_match.next_in_group.should be @third_match
           end
           it "should leave the first_match in the matches array" do
-            @matches.should include (@first_match)
+            @matches.should include(@first_match)
           end
           it "should delete the second and third match from the matches" do
             @matches.should_not include(@second_match)
@@ -107,7 +107,8 @@ module HardCiter
 
       context "with a line that has three matches, with two in a row" do
         before do
-          @line = "{match:one}{match:two} something that is not a citation {match:three}"
+          @line = "This sentence has two paired citations{Davis:2003wp}{Biswal:2010fj} and 
+                  a lone citation{Jones:1999qs}."
           @matches = Parser.new.parse_line(@line)
           @first_match = @matches[0]
           @second_match = @matches[1]
@@ -129,39 +130,3 @@ module HardCiter
     end
   end
 end
-
-#Group matches spec old
-
-
-#describe HardCiter, "#group_matches" do
-#  let(:hard_citer){ HardCiter.new }
-#  let(:two_paired_matches_line) { "This sentence has two citations at the end{Biswal:2010fj}{Davis:2003wp}." }
-#  let(:two_unpaired_matches_line) { "This sentence has two unpaired{Biswal:2010fj} citations{Davis:2003wp}." }
-#  let(:one_separate_one_pair_line) { "This sentence has two paired citations{Davis:2003wp}{Biswal:2010fj} and "+
-#                                    "a lone citation{Jones:1999qs}." }
-#  let(:biswal_citation) { Citation.new("{Biswal:2010fj}") }
-#  let(:jones_citation) { Citation.new("{Jones:1999qs}") }
-#  let(:davis_citation) { Citation.new("{Davis:2003wp}") }
-#
-#  describe "when given a line with two matches not next to each other" do
-#    it "should return an array with two elements (one for each match)" do
-#      matches = [[biswal_citation, 30], [davis_citation, 55]]
-#      pairing_result = hard_citer.group_matches(matches, two_unpaired_matches_line)
-#      pairing_result.should == [[biswal_citation, 30], [davis_citation, 55]]
-#    end
-#  end
-#  describe "when given a line with two matches next to each other" do
-#    it "should return an array with one element: an array of the two matches" do
-#      matches = [[biswal_citation, 42], [davis_citation, 57]]
-#      pairing_result = hard_citer.group_matches(matches, two_paired_matches_line)
-#      pairing_result.should == [[[biswal_citation, 42], [davis_citation, 57]]]
-#    end
-#  end
-#  describe "when given a line with one pair of matches and one lone match" do
-#    it "should return an array with two elements: an array of the two matches, and the lone match" do
-#      matches = [[davis_citation, 38], [biswal_citation, 52], [jones_citation, 87]]
-#      pairing_result = hard_citer.group_matches(matches, one_separate_one_pair_line)
-#      pairing_result.should == [[[davis_citation, 38], [biswal_citation, 52]], [jones_citation, 87]]
-#    end
-#  end
-#end
